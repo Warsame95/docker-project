4200b5cef1e5efb2152e9cc7db78bcb3eaa90357
Warsame <warsameahmed95@gmail.com>
2025-08-01 15:40:29 +0100

    Edited the functions to render the templates

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
 import os
 
@@ -9,19 +9,12 @@ r = Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 @app.route("/")
 def index():
-    html = """
-    <h1>Welcome to my flask app.</h1>
-    <form action="/count">
-             <button type="submit">Go to Counter</button>
-    </form>
-    """
-    return html
+    return render_template("welcome.html")
 
 @app.route("/count")
 def count():
     visits = r.incr('visits')
-    print(r.get('visits'))
-    return f"""<h1>{visits} visits</h1>"""
+    return render_template("count.html", count=visits)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 5002)
